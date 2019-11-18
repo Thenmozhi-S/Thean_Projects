c0a4634a0e7aa1d9b24c6eaeb3edd7e8ceb9dfed
ThenmozhiSubramani <ThenmozhiSubramani@DESKTOP-T0N5L5P>
2019-11-18 10:52:19 +0530

    Added drivers and created a one test file for magento site

---
+++ final-framework-testng/Assignments/Simple_Pack/Test1.java
@@ -0,0 +1,22 @@
+package Simple_Pack;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Test1
+{
+@Test
+public void magentoLogin()
+{
+	WebDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	driver.get("http://magento.com");
+	driver.findElement(By.className("fa-user")).click();
+	driver.quit();
+}
+}
